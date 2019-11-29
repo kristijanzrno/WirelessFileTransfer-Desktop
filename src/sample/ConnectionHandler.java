4d9bcfdf5dd133ccfd8db20aa505a7c6828120d9
Kristijan Zrno <kristijanzrno@Kristijans-iMac.local>
2019-11-29 14:57:06 +0000

    Initial commit

---
+++ src/sample/ConnectionHandler.java
@@ -0,0 +1,81 @@
+package sample;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ConnectionHandler extends Thread{
+    private ServerSocket serverSocket;
+    private Socket androidDevice;
+    private int port = 9270;
+    private boolean isRunning = true;
+
+    DataInputStream input = null;
+    DataOutputStream output = null;
+
+    public ConnectionHandler(){
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+            isRunning = false;
+        }
+    }
+
+    @Override
+    public void run() {
+        //super.run();
+
+        while(isRunning){
+            try {
+                androidDevice = serverSocket.accept();
+                if(androidDevice != null)
+                    break;
+            } catch (IOException e) {
+                e.printStackTrace();
+                isRunning = false;
+            }
+        }
+        System.out.println("Connected");
+        try {
+            input = new DataInputStream(androidDevice.getInputStream());
+            output = new DataOutputStream(androidDevice.getOutputStream());
+
+            while (isRunning) {
+                String androidMessage = receiveMessage();
+                if(androidMessage !=null && !androidMessage.isEmpty())
+                    System.out.println(androidMessage);
+                //sendMessage("");
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private String receiveMessage(){
+        if(input != null){
+            try {
+                return input.readUTF();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "Could not read data";
+            }
+        }
+        return "";
+    }
+    public void sendMessage(String message){
+        if(output != null){
+            try {
+                output.writeUTF(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void stopConnection(){
+        isRunning = false;
+    }
+}
