4b60b8b359c3729b8e538895ca947d01b2ebc3f9
kristijanzrno <superkiki.info.android@gmail.com>
2020-04-15 02:35:19 +0200

    Receiving files successfully

--- src/filetransfer/ConnectionHandler.java
+++ src/filetransfer/ConnectionHandler.java
@@ -1,22 +1,20 @@
 package filetransfer;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.Inet4Address;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class ConnectionHandler extends Thread{
+public class ConnectionHandler extends Thread {
     private ServerSocket serverSocket;
     private Socket androidDevice;
     private int port = 9270;
     private boolean isRunning = true;
+    private boolean receiving;
 
     DataInputStream input = null;
     DataOutputStream output = null;
 
-    public ConnectionHandler(){
+    public ConnectionHandler() {
         try {
             serverSocket = new ServerSocket(port);
         } catch (IOException e) {
@@ -27,33 +25,54 @@ public class ConnectionHandler extends Thread{
 
     @Override
     public void run() {
-        while(isRunning){
+        while (isRunning) {
             try {
                 androidDevice = serverSocket.accept();
-                if(androidDevice != null)
+                if (androidDevice != null)
                     break;
             } catch (IOException e) {
                 e.printStackTrace();
                 isRunning = false;
             }
         }
-        System.out.println("Connected");
+        System.out.println("Connected...");
         try {
             input = new DataInputStream(androidDevice.getInputStream());
             output = new DataOutputStream(androidDevice.getOutputStream());
 
             while (isRunning) {
-                String androidMessage = receiveMessage();
-                if(androidMessage !=null && !androidMessage.isEmpty())
-                    System.out.println(androidMessage);
+                String receivedData = receiveMessage();
+                String[] message = receivedData.split(Constants.DATA_SEPARATOR);
+                if (!receivedData.isEmpty())
+                    System.out.println(message[0]);
+                switch (message[0]) {
+                    case Constants.FILE_SEND_MESSAGE:
+                        System.out.println("Preparing to receive data. Receiving " + message[1] + " items.");
+                        break;
+                    case Constants.FILE_NAME_MESSAGE:
+                        String filename = message[1];
+                        long fileSize = Long.parseLong(message[2]);
+                        System.out.println("Receiving " + filename + "...");
+                        receiveFile(filename, fileSize, input);
+                        try {
+                            Thread.sleep(10000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case Constants.FILE_SEND_COMPLETE_MESSAGE:
+                        break;
+
+                }
+
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private String receiveMessage(){
-        if(input != null){
+    private String receiveMessage() {
+        if (input != null) {
             try {
                 return input.readUTF();
             } catch (IOException e) {
@@ -63,8 +82,35 @@ public class ConnectionHandler extends Thread{
         }
         return "";
     }
-    public void sendMessage(String message){
-        if(output != null){
+
+    private boolean receiveFile(String filename, long fileSize, InputStream inputStream) {
+        receiving = true;
+        File file = new File(filename);
+        System.out.println("receiving....");
+        try {
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+
+            byte[] buffer = new byte[8192];
+            int count = 0;
+            while (fileSize > 0 && (count = inputStream.read(buffer, 0, (int) Math.min(buffer.length, fileSize))) > 0) {
+                bufferedOutputStream.write(buffer, 0, count);
+                fileSize -= count;
+            }
+            bufferedOutputStream.close();
+            //inputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        receiving = false;
+        System.out.println("done....");
+        return true;
+    }
+
+    public void sendMessage(String message) {
+        if (output != null) {
             try {
                 output.writeUTF(message);
             } catch (IOException e) {
---
+++ src/filetransfer/Constants.java
@@ -0,0 +1,11 @@
+package filetransfer;
+
+public class Constants {
+    // DESKTOP SPECIFIC CONSTANTS
+
+    // GENERAL CONSTANTS
+    public static final String FILE_SEND_MESSAGE = "_transferring";
+    public static final String FILE_SEND_COMPLETE_MESSAGE = "_complete";
+    public static final String FILE_NAME_MESSAGE = "_filename";
+    public static final String DATA_SEPARATOR = ":";
+}
